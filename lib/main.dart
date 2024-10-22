13c32bd944cffebf41ab683b6fd794903be223a6
USER <afsanamim5220@gamil.com>
2024-10-22 16:58:50 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,21 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
-import 'package:my_practice/currency_converter_cupertino_page.dart';
-import 'package:my_practice/currency_converter_materialpage.dart';
+import 'package:my_practice/weather_screen.dart';
+
 
 void main(){
-  runApp( const MyCupertinoApp());
+  runApp( const MyApp());
 }
-// class MyApp extends StatelessWidget {
-//    const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return  const MaterialApp(
-//       home:CurrencyConverterMaterialpage() ,
-//     );
-//   }
-// }
-class MyCupertinoApp extends StatelessWidget {
-  const MyCupertinoApp({super.key});
+class MyApp extends StatelessWidget {
+   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
-      home: CurrencyConverterCupertinoPage(),
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:const WeatherScreen() ,
+      theme: ThemeData.dark(useMaterial3: true),
     );
   }
 }
+
